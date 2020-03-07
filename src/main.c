e8beda363c1c805af25752f68af2343e3e710734
Ben Burkert <ben@benburkert.com>
2020-03-07 01:43:00 +0000

    src: add skeleton main & basic makefile

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <net/tcp.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Ben Burkert");
+MODULE_DESCRIPTION("TODO");
+MODULE_VERSION("0.1");
+MODULE_ALIAS_TCP_ULP("socketguard");
+
+static int ulp_init(struct sock *sk)
+{
+	return 0;
+}
+
+static struct tcp_ulp_ops sg_tcp_ulp_ops __read_mostly = {
+	.name			= "socketguard",
+	.owner			= THIS_MODULE,
+	.init			= ulp_init,
+};
+
+static int __init mod_init(void)
+{
+	tcp_register_ulp(&sg_tcp_ulp_ops);
+	return 0;
+}
+
+static void __exit mod_exit(void)
+{
+	tcp_unregister_ulp(&sg_tcp_ulp_ops);
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
